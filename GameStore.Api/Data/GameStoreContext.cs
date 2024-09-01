b77789dc7aacd1463a1578fec1fbe4cbf7bdbe56
Kristian <10221425@ucn.dk>
2024-09-01 18:02:22 +0200

    Seeding data

--- GameStore.Api/Data/GameStoreContext.cs
+++ GameStore.Api/Data/GameStoreContext.cs
@@ -2,16 +2,24 @@
 using GameStore.Api.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace GameStore.Api.Data
+namespace GameStore.Api.Data;
+
+public class GameStoreContext(DbContextOptions<GameStoreContext> options)
+    : DbContext(options)
 {
-    public class GameStoreContext(DbContextOptions<GameStoreContext> options)
-         : DbContext(options)
+    // Create the Games table
+    public DbSet<Game> Games => Set<Game>();
+    // Create the Genres table
+    public DbSet<Genre> Genres => Set<Genre>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        // Create the Games table
-        public DbSet<Game> Games => Set<Game>();
-        // Create the Genres table
-        public DbSet<Genre> Genres => Set<Genre>();
-
-
+        modelBuilder.Entity<Genre>().HasData(
+            new { Id = 1, Name = "Fighting" },
+            new { Id = 2, Name = "Roleplaying" },
+            new { Id = 3, Name = "Sports" },
+            new { Id = 4, Name = "Racing" },
+            new { Id = 5, Name = "Kids and Family" }
+        );
     }
 }
